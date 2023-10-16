01629b0e7e8989120c871485b546f1373107d458
Will Munslow <will@subterrane.com>
2023-10-16 11:46:05 -0700

    simplify logging

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 export default function handler(req, res) {
-  console.log("-------\n", new Date().toISOString(), req.method, req.url);
   const { body } = req;
-  
+
   try {
     console.log(JSON.parse(body));
   } catch {
     console.log(body);
   }
-
+  
   res.status(200).end();
 }
